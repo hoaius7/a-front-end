3ea75bbef61ec838fbf8ba597cd1cf40ce328fa3
Hoai Nguyen <hoaius7@gmail.com>
2019-02-19 11:41:11 -0600

    Routing - Setting Up the Router Package, Preparing the Project For Routing
    npm install --save react-router-dom

--- react/react-ajax-routing/src/containers/Blog/Blog.js
+++ react/react-ajax-routing/src/containers/Blog/Blog.js
@@ -1,10 +1,7 @@
-import React, { Component } from 'react';
-// import axios from 'axios';
-import axios from '../../axios';
+import React, {Component} from 'react';
 
-import Post from '../../components/Post/Post';
-import FullPost from '../../components/FullPost/FullPost';
-import NewPost from '../../components/NewPost/NewPost';
+import Posts from './Posts/Posts';
+
 import './Blog.css';
 
 class Blog extends Component {
@@ -14,44 +11,7 @@ class Blog extends Component {
         error: false
     }
 
-    componentDidMount() {
-        axios.get('/posts')
-            .then(response => {
-                const posts = response.data.slice(0, 4);
-                const updatedPosts = posts.map(post => {
-                    return {
-                        ...post,
-                        author: 'Hoai'
-                    }
-                });
-                this.setState({
-                    posts: updatedPosts
-                });
-            })
-            .catch(error => {
-                this.setState({error: true});
-            });
-    }
-
-    postSelectedHandler = (id) => {
-        this.setState({
-            selectedPostId: id
-        });
-    }
-
-    render () {
-        let posts = <p style={{textAlign: 'center'}}>Something went wrong!</p>;
-        if (!this.state.error) {
-            posts = this.state.posts.map(post =>
-                <Post
-                    key={post.id}
-                    title={post.title}
-                    author={post.author}
-                    clicked={() => this.postSelectedHandler(post.id)}
-                />
-            );
-        }
-
+    render() {
         return (
             <div className="Blog">
                 <header>
@@ -62,15 +22,7 @@ class Blog extends Component {
                         </ul>
                     </nav>
                 </header>
-                <section className="Posts">
-                    {posts}
-                </section>
-                <section>
-                    <FullPost id={this.state.selectedPostId} />
-                </section>
-                <section>
-                    <NewPost />
-                </section>
+                <Posts />
             </div>
         );
     }
